2856f581f5453ca84debf9bc06cf0a455496495c
Karol Zak <karol.zak@hotmail.com>
2020-06-02 15:30:16 +0200

    mov config.py

--- box_detector/config.py
+++ box_detector/config.py
@@ -5,4 +5,4 @@ min_h, max_h = (50, 60)
 wh_ratio_range = (0.65, 0.9)
 padding = 1
 thickness = 2
-processing_time_widths = [800, 1000, 1300, 1400, 1600, 2600]
+scaling_factors = [0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
