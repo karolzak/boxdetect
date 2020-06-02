0dcd7683ada969729cc168a2a0b219bc4ef438ac
Karol Zak <karol.zak@hotmail.com>
2020-06-02 12:53:59 +0200

    upd helpers add config

---
+++ config.py
@@ -0,0 +1,8 @@
+# parameters
+min_w, max_w = (40, 50)
+min_h, max_h = (50, 60)
+
+wh_ratio_range = (0.65, 0.9)
+padding = 1
+thickness = 2
+processing_time_widths = [800, 1000, 1300, 1400, 1600, 2600]
