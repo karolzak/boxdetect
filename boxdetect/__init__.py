7daaa8e690330f10a38312247eebae9ab529ad6b
Karol Zak <karol.zak@hotmail.com>
2020-06-08 13:22:30 +0200

    upd prep for release

---
+++ boxdetect/__init__.py
@@ -0,0 +1,3 @@
+name = "boxdetect"
+
+__version__ = "0.1.1"
--- boxdetect/config.py
+++ boxdetect/config.py
@@ -9,7 +9,7 @@ wh_ratio_range = (0.65, 0.1)
 # but also it takes more time to processing.
 # Too small scaling factor may cause false positives
 # Too big scaling factor will take a lot of processing time
-scaling_factors = [0.5, 0.7]
+scaling_factors = [0.5]
 
 # Rectangle kernel padding
 padding = 1
@@ -20,12 +20,12 @@ thickness = 2
 # Image enhancement
 dilation_kernel = (2, 2)
 # Num of iterations when running dilation tranformation (to engance the image)
-dilation_iterations = 2
+dilation_iterations = 0
 
 # Rectangles grouping
-group_size_range = (2, 100)  # minimum number of rectangles in a group, >2 - will ignore groups with single rect  # NOQA E501
-vertical_max_distance = min_h * 0.3  # in pixels
+group_size_range = (1, 100)  # minimum number of rectangles in a group, >2 - will ignore groups with single rect  # NOQA E501
+vertical_max_distance = 10  # in pixels
 # Multiplier to be used with mean width of all the rectangles detected
 # E.g. for multiplier of 4 the maximum distance between boxes to be grouped together will be:  # NOQA E501
 # max_horizontal_distance = np.mean(all_rect_widths) * 4
-horizontal_max_distance_multiplier = 4
+horizontal_max_distance_multiplier = 2
