49b552b3b1aba415228514f16649006345641017
Karol Zak <karol.zak@hotmail.com>
2020-06-10 22:09:45 +0200

    add morph_kernel variables to config

--- boxdetect/config.py
+++ boxdetect/config.py
@@ -6,9 +6,6 @@ class PipelinesConfig:
     # w/h ratio range for boxes/rectangles filtering
     wh_ratio_range = (0.65, 0.1)
 
-    # Rectangular kernels border thickness
-    border_thickness = 1
-
     # The more scaling factors the more accurate the results
     # but also it takes more time to processing.
     # Too small scaling factor may cause false positives
@@ -18,10 +15,16 @@ class PipelinesConfig:
     # Drawing rectangles
     thickness = 2
 
-    # Image enhancement
+    # Image processing
     dilation_kernel = (2, 2)
     # Num of iterations when running dilation tranformation (to engance the image)  # NOQA E501
     dilation_iterations = 0
+
+    morph_kernels_type = 'lines'  # 'rectangles'
+    morph_kernels_lines_length = 15
+    morph_kernels_lines_thickness = 1
+    # Rectangular kernels border thickness
+    border_thickness = 1
 
     # Rectangles grouping
     group_size_range = (1, 100)  # minimum number of rectangles in a group, >2 - will ignore groups with single rect  # NOQA E501
@@ -29,4 +32,4 @@ class PipelinesConfig:
     # Multiplier to be used with mean width of all the rectangles detected
     # E.g. for multiplier of 4 the maximum distance between boxes to be grouped together will be:  # NOQA E501
     # max_horizontal_distance = np.mean(all_rect_widths) * 3
-    horizontal_max_distance_multiplier = 3
+    horizontal_max_distance = width_range[0] * 2
